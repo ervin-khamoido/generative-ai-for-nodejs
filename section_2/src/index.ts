b797d73a9f1df566de8ea31edcd09f1baa6e6674
Ervin Khamoido <ervin.khamoido@gmail.com>
2024-12-21 01:22:47 +0100

    basic setup

---
+++ section_2/src/index.ts
@@ -0,0 +1,24 @@
+import { OpenAI } from 'openai';
+import * as dotenv from 'dotenv';
+import { encoding_for_model } from 'tiktoken';
+
+dotenv.config();
+const openai = new OpenAI()
+
+async function main() {
+  const response = await openai.completions.create({
+    model: 'gpt-3.5-turbo-instruct',
+    prompt: 'How tall is mount everest?',
+  });
+
+  console.log(response.choices[0].text);
+}
+
+function encodePrompt() {
+  const prompt = 'How tall is mount everest?';
+  const encoder = encoding_for_model("gpt-3.5-turbo-instruct");
+  const words = encoder.encode(prompt);
+  console.log(words);
+}
+
+encodePrompt();
